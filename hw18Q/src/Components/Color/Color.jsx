d40d4b248b2b15cc4b1225edac1883f78b71bb18
fahimehsd <fahimehsadeghi3@gmail.com>
2023-02-06 21:13:33 +0330

    q1 done

--- hw18Q/src/Components/Color/Color.jsx
+++ hw18Q/src/Components/Color/Color.jsx
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
-import "./Color.css";
-export default function Color() {
+import "./color.css";
+
+function Color() {
   const [colors, setColors] = useState([]);
   const addColor = () => {
-    const color =
-      "rgb(" +
-      Math.floor(Math.random() * 256) +
-      "," +
-      Math.floor(Math.random() * 256) +
-      "," +
-      Math.floor(Math.random() * 256) +
-      ")";
+    const x = Math.floor(Math.random() * (225 - 0 + 1)) + 0;
+    const y = Math.floor(Math.random() * (225 - 0 + 1)) + 0;
+    const z = Math.floor(Math.random() * (225 - 0 + 1)) + 0;
+    const color = `rgb(${x},${y},${z})`;
     setColors([color, ...colors]);
   };
   return (
-    <div>
+    <div className="colorContainer">
       <button onClick={addColor}>Add Color</button>
-      <div className="pickedColor">
-        {colors.length > 0 &&
-          colors.map((clr) => {
+
+      {colors.length > 0 &&
+        colors.map((color) => (
+          <div style={{ display: "flex", alignItems: "center" }}>
             <div
-              className="colorBox"
-              key={clr}
-              style={{ backgroundColor: `${clr}` }}
-            >
-              {clr}
-            </div>;
-          })}
-      </div>
+              key={color}
+              style={{
+                background: `${color}`,
+                width: "5rem",
+                height: "5rem",
+                margin: "1rem"
+              }}
+            ></div>
+            <p>{color}</p>
+          </div>
+        ))}
     </div>
   );
 }
+
+export default Color;
